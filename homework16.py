6e9f2f2e24fb9fdf2cf8e2e9da685f7f114dac56
shumerex <163420582+shumerex@users.noreply.github.com>
2024-04-26 19:18:17 +0700

    Add files via upload

--- homework16.py
+++ homework16.py
@@ -9,7 +9,6 @@ class Car:
 
 class Nissan(Car):
     def __init__(self):
-        super().__init__()
         self.price = 1500000
 
 
@@ -19,22 +18,20 @@ class Nissan(Car):
 
 class Kia(Car):
     def __init__(self):
-        super().__init__()
         self.price = 1200000
 
     def horse_powers(self):
         return "250 лошадиных сил"
 
 
-if __name__ == "__main__":
-    car = Car()
-    print(f"Цена базового автомобиля: {car.price}")
-    print(f"Количество лошадиных сил базового автомобиля: {car.horse_powers()}")
+car = Car()
+print(f"Цена базового автомобиля: {car.price}")
+print(f"Количество лошадиных сил базового автомобиля: {car.horse_powers()}")
 
-    nissan_car = Nissan()
-    print(f"Цена автомобиля Nissan: {nissan_car.price}")
-    print(f"Количество лошадиных сил автомобиля Nissan: {nissan_car.horse_powers()}")
+nissan_car = Nissan()
+print(f"Цена автомобиля Nissan: {nissan_car.price}")
+print(f"Количество лошадиных сил автомобиля Nissan: {nissan_car.horse_powers()}")
 
-    kia_car = Kia()
-    print(f"Цена автомобиля Kia: {kia_car.price}")
-    print(f"Количество лошадиных сил автомобиля Kia: {kia_car.horse_powers()}")
+kia_car = Kia()
+print(f"Цена автомобиля Kia: {kia_car.price}")
+print(f"Количество лошадиных сил автомобиля Kia: {kia_car.horse_powers()}")
